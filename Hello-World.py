8b5fe720fa29c4ad4df1082633d2583534dd4491
gsbeshkov <31181546+gsbeshkov@users.noreply.github.com>
2021-09-30 12:48:36 +0300

    Update Hello-World.py

--- Hello-World.py
+++ Hello-World.py
@@ -1,14 +1,15 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-
+import os
 app = Flask(__name__)
 api = Api(app)
 
+var = 'CONTAINER_PYTHON_VAR'
+value = os.getenv(var)
 class Greeting (Resource):
-    def get(self):
-        return 'Hello World!'
-
+def get(self):
+return value
 api.add_resource(Greeting, '/') # Route_1
 
 if __name__ == '__main__':
-    app.run('0.0.0.0','3333')
+app.run('0.0.0.0','31313')
